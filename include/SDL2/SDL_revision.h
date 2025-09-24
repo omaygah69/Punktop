120a63b854b7a45dc3ae74c7e2845747c5e5394a
omaygah69 <robertjohngarcia090604@gmail.com>
2025-09-25 01:18:05 +0800

    the kambing is back

---
+++ include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL2COMPAT_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL2COMPAT_VENDOR_INFO
+#define SDL_REVISION "release-2.32.56-0-g1321b6c (" SDL2COMPAT_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "release-2.32.56-0-g1321b6c"
+#endif
